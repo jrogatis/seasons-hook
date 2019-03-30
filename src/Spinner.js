b710ef518eaa2af868d2328dc54ff3b05da2d737
Jean <jean.rogatis@sysmap.com.br>
2019-03-30 00:50:56 -0300

    initial

---
+++ src/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Spinner = ({ message }) => {
+  return (
+    <div class="ui active dimmer">
+      <div class="ui text loader">{message}</div>
+    </div>
+  );
+};
+
+Spinner.defaultProps = {
+  message: 'loading...'
+};
+
+export default Spinner;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
+
+  const renderContent = () => {
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>;
+    }
+    if (!errorMessage && lat) {
+      return <SeasonDisplay lat={lat} />;
+    }
+    return <Spinner message="Accept location request" />;
+  };
+  return <div className="border red"> {renderContent()}</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
